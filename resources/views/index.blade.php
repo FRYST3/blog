269c9074e7788209ec064a557e488492244fc28d
FRYST <bloodgrab317@yandex.by>
2024-06-19 16:12:00 +0300

    Формирование структуры для frontend

---
+++ resources/views/index.blade.php
@@ -0,0 +1,18 @@
+<!doctype html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+    <link rel="stylesheet" href="/assets/css/style.css">
+
+    <script src="/assets/js/components/jquery.min.js"></script>
+    <script src="/assets/js/app.js"></script>
+
+    <title>Blog || FRYST</title>
+</head>
+<body>
+
+</body>
+</html>
